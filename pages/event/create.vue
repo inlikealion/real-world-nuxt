78684f35acf78b5d0d51c309733613ba401aa74a
Matt Bainton <matt@inlikealion.com>
2019-08-01 09:54:02 -0400

    8. Using Vuex - complete

---
+++ pages/event/create.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Creat an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create a new event in your neighborhood'
+        }
+      ]
+    }
+  }
+}
+</script>
